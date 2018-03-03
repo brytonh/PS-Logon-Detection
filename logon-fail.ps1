1f834cc13dc478b9a5da1eead706d4ff4755412a
bjherdes1s <bjherdes1s@semo.edu>
2018-03-02 18:36:03 -0600

    Initial

--- logon-fail.ps1
+++ logon-fail.ps1
@@ -1,7 +1,7 @@
 ﻿
 #.'C:\Users\Public\crypto.ps1'
 
-$message=Get-EventLog -LogName Security -Computer ANONMOUSE-2 -InstanceID 4625 | select message | format-list | findstr /c:"Account Name" | Sort-Object | Get-Unique
+$message=Get-EventLog -LogName Security -Computer $env:computername -InstanceID 4625 | select message | format-list | findstr /c:"Account Name" | Sort-Object | Get-Unique
 
 #Write-Output $message #full message with accountname: username format
 
@@ -21,7 +21,7 @@ if($message -ne $null) {
 	$password=ConvertTo-SecureString $env:pass -AsPlainText -Force
 
 	$creds= New-Object System.Management.Automation.PSCredential ($SendAddress,$password) 
-	$user=gwmi -class Win32_ComputerSystem | select username | grep ANON | %{ $_.Split('\')[1]; }
+	$user=gwmi -class Win32_ComputerSystem | select username | grep $env:computername | %{ $_.Split('\')[1]; }
 
 	Send-MailMessage -From $SendAddress -to "6186767971@vtext.com" -Subject "Logon Failure" `
 	-Body " $tmp" `
